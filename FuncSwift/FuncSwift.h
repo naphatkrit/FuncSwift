2777308bccc0d3cf571d095e7dd51253adbdf61e
Naphat Sanguansin <naphat.krit@gmail.com>
2015-01-28 13:39:38 -0500

    Initial Commit

---
+++ FuncSwift/FuncSwift.h
@@ -0,0 +1,19 @@
+//
+//  FuncSwift.h
+//  FuncSwift
+//
+//  Created by Naphat Sanguansin on 1/28/15.
+//  Copyright (c) 2015 Naphat Sanguansin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FuncSwift.
+FOUNDATION_EXPORT double FuncSwiftVersionNumber;
+
+//! Project version string for FuncSwift.
+FOUNDATION_EXPORT const unsigned char FuncSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FuncSwift/PublicHeader.h>
+
+
